f9eb40cd4ac1bae5cc69f3acb242aa66385bda31
aothymoon59 <aothymoon59@gmail.com>
2026-03-18 14:27:02 +0600

    :sparkles: added catch all route

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,6 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import AdminPanel from "../pages/AdminPanel";
 
+// Simple NotFound component
+const NotFound = () => (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>404 - Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+    </div>
+);
+
 export const router = createBrowserRouter([
     {
         path: "/",
@@ -10,5 +18,9 @@ export const router = createBrowserRouter([
     {
         path: "/admin",
         element: <AdminPanel />,
+    },
+    {
+        path: "*",
+        element: <NotFound />,
     },
 ]);
