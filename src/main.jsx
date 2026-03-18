973309d131c54b61c5dc9d31b64f180f56f07e68
aothymoon59 <aothymoon59@gmail.com>
2026-03-18 14:23:29 +0600

    :recycle: modify routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import AdminPanel from "./pages/AdminPanel.jsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { router } from "./router/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
